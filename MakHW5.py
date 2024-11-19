eba6d9fb6e34c6acad6956fd2e6bbd6c7efd7a9f
cmakowsk <cmakowsk@uwyo.edu>
2024-11-19 18:02:08 +0000

    We started HW

--- MakHW5.py
+++ MakHW5.py
@@ -1,2 +1,33 @@
 import openpyxl
- 
+wb = openpyxl.Workbook()
+from openpyxl.styles import PatternFill
+b = "1E3A5F"
+r = "D32F2F"
+br = "6E4B3A"
+t = "F1C27D"
+y = "FFEB3B"
+
+pixel_art = [
+[b, b, b, b, b, b, b, b, b, b, b, b, b, b]
+[b, b, b, b, r, r, r, r, r, b, b, b, b, b]
+[b, b, b, r, r, r, r, r, r, r, r, r, b, b]
+[b, b, b, br, br, br, t, t, br, t, b, b, b, b]
+[b, b, br, t, br, t, t, t, br, t, t, t, b, b]
+[b, b, br, t, br, br, t, t, t, br, t, t, t, b]
+[b, b, br, br, t, t, t, t, br, br, br, br, b, b]
+[b, b, b, t, t, t, t, t, t, t, t, b, b, b]
+[b, b, b, br, br, r, br, br, r, b, b, b, b, b]
+[b, b, br, br, br, r, r, r, r, br, br, br, b, b]
+[b, br, br, br, r, y, r, r, y, r, ]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+
+]
+
+
+wb.save("PixelArt.xlsx")
